c866b6117fc5dc46ba7be92baad8de846a87aed1
yukke271 <yukke271@gmail.com>
2024-07-15 03:25:28 +0900

    alignment completed

--- app/vrc_favo_lister/astro.config.mjs
+++ app/vrc_favo_lister/astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    domains: ["vrchat.com"],
+  },
+  integrations: [solidJs()],
+});
