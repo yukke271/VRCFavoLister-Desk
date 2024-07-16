f31dcf74a5329bf2b98ee99a094020b65edfa01b
yukke271 <yukke271@gmail.com>
2024-07-16 17:31:38 +0900

    update config type

---
+++ app/vrc_favo_lister/src/types/config.ts
@@ -0,0 +1,11 @@
+// 設定ファイルの構造
+
+export interface Config {
+  baseUrl: string;
+  apiKey: string;
+  userAgent: string;
+  authCookie: string;
+  basicCredentials: string;
+  username: string;
+  password: string;
+}
