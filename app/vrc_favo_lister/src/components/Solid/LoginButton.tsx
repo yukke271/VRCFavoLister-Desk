0d7b45c105b37d3cbda7e7349f003e681b1c5363
yukke271 <yukke271@gmail.com>
2024-07-18 02:37:54 +0900

    update flontend / Refactoring

--- app/vrc_favo_lister/src/components/Solid/LoginButton.tsx
+++ app/vrc_favo_lister/src/components/Solid/LoginButton.tsx
@@ -1,25 +1,24 @@
 import { createSignal } from "solid-js";
 import { invoke } from "@tauri-apps/api";
 
-export default function TestButton() {
+export default function LoginButton() {
   const [username, setUsername] = createSignal("");
   const [password, setPassword] = createSignal("");
   const [otpCode, setOtpCode] = createSignal("");
-  const [testMsg, setTestMsg] = createSignal("");
+  const [statusCode, setStatusCode] = createSignal("");
 
-  async function testButton() {
-    invoke("login", { 
+  async function loginButton() {
+    setStatusCode(await invoke("login", { 
       username: username(),
       password: password(),
       otpCode: otpCode()
-    }).then((result) => {
-      // 1が返ってきたらusernameとpasswordの入力を求めるダイアログを出す
-
-      setTestMsg(result as string);
-    });
+    }) as string);
   }
+
   /*
-   エラーコードを返す
+    app\vrc_favo_lister\src-tauri\src\apiconfig.rs
+
+    以下のエラーコードが返される
     0: 成功
     1: なんらかの処理に失敗した
     2: usernameかpasswordが空
@@ -31,33 +30,33 @@ export default function TestButton() {
 
   return (
     <>
-        <button onClick={testButton}>testButton</button>
+        <button onClick={loginButton}>Login</button>
 
-        <div {...(testMsg() == "0" ? {} : {class: "none-visible"})} >
+        <div {...(statusCode() == "0" ? {} : {class: "none-visible"})} >
           <p> Login Success!! </p>
         </div>
 
-        <div {...(testMsg() == "1" ? {} : {class: "none-visible"})} >
+        <div {...(statusCode() == "1" ? {} : {class: "none-visible"})} >
           <p> Login Failed!! </p>
         </div>
 
-        <div {...(testMsg() == "2" ? {} : {class: "none-visible"})} >
+        <div {...(statusCode() == "2" ? {} : {class: "none-visible"})} >
           <p> ユーザーIDもしくはメールアドレスと、パスワードを入力してください </p>
           <input onChange={(e) => setUsername(e.currentTarget.value)} type="text" placeholder="enter a username or email ..."/>
           <input onChange={(e) => setPassword(e.currentTarget.value)} type="password" placeholder="enter a password ..."/>
         </div>
 
-        <div {...(testMsg() == "3" ? {} : {class: "none-visible"})} >
+        <div {...(statusCode() == "3" ? {} : {class: "none-visible"})} >
           <p> 2段階認証コードをメールボックスから確認してください </p>
           <input onChange={(e) => setOtpCode(e.currentTarget.value)} type="text" pattern="[0-9]" maxlength="6"  placeholder="enter a one time password ..."/>
         </div>
         
-        <div {...(testMsg() == "4" ? {} : {class: "none-visible"})} >
+        <div {...(statusCode() == "4" ? {} : {class: "none-visible"})} >
           <p> 2段階認証コードを認証アプリから確認してください </p>
           <input onChange={(e) => setOtpCode(e.currentTarget.value)} type="text" pattern="[0-9]" maxlength="6" placeholder="enter a one time password ..."/>
         </div>
 
-        <div {...(testMsg() == "5" ? {} : {class: "none-visible"})} >
+        <div {...(statusCode() == "5" ? {} : {class: "none-visible"})} >
           <p> 前回の入力時に空でした。<br /> 6桁の正確な2段階認証コードを入力してください </p>
           <input onChange={(e) => setOtpCode(e.currentTarget.value)} type="text" pattern="[0-9]" maxlength="6" placeholder="enter a one time password ..."/>
         </div>
