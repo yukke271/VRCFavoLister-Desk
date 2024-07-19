06db7f532c707c664b962d04c1968e5ce3cc1e5a
yukke271 <yukke271@gmail.com>
2024-07-19 22:38:32 +0900

    Enable hot reloading

--- app/vrc_favo_lister/astro.config.mjs
+++ app/vrc_favo_lister/astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    domains: ["vrchat.com"],
+  vite: {
+    server: {
+      watch: {
+        // viteの設定からポーリングを有効にすることでWSL外部のファイル監視を有効にする
+        usePolling: true
+      }
+    }
   },
-  integrations: [solidJs()],
+  image: {
+    domains: ["vrchat.com"]
+  },
+  integrations: [solidJs(), tailwind()]
 });
