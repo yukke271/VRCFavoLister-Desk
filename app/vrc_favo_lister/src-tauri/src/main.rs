ad31ea852f0e42dd3c66952bebb86c5988ba2555
yukke271 <yukke271@gmail.com>
2024-07-22 22:07:05 +0900

    update frontend / update main module

--- app/vrc_favo_lister/src-tauri/src/main.rs
+++ app/vrc_favo_lister/src-tauri/src/main.rs
@@ -3,14 +3,27 @@
 
 mod commands;
 mod structs;
+mod database;
+
+use tauri::{Manager, async_runtime::block_on};
+use database::sqlitedb::init_db_pool;
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![
-        commands::auth::login,
-    ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    
+    // DBの初期化
+    let db_pool = block_on(init_db_pool()).expect("error while initializing database pool");
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            commands::auth::login,
+            commands::favorite::load_favorite,
+        ])
+        .setup(|app| {
+            app.manage(db_pool);
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 /*
