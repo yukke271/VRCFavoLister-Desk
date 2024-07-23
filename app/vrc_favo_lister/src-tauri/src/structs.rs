b6d06ef2306b7bc164589c349bad84d08cfebc31
yukke271 <yukke271@gmail.com>
2024-07-23 21:51:57 +0900

    update backend / create structs

--- app/vrc_favo_lister/src-tauri/src/structs.rs
+++ app/vrc_favo_lister/src-tauri/src/structs.rs
@@ -1,1 +1,3 @@
 pub mod apiconfig;
+pub mod unity_package_from_api;
+pub mod favorite_world_from_api;
---
+++ app/vrc_favo_lister/src-tauri/src/structs/favorite_world_from_api.rs
@@ -0,0 +1,32 @@
+use serde::{Serialize, Deserialize};
+use crate::structs::unity_package_from_api::UnityPackageFromAPI;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FavoriteWorldFromAPI {
+    #[serde(rename = "id")]
+    pub world_id: String,
+    #[serde(rename = "name")]
+    pub world_name: String,
+    #[serde(rename = "description")]
+    pub description: Option<String>,
+    #[serde(rename = "authorName")]
+    pub author_name: String,
+    #[serde(rename = "releaseStatus")]
+    pub release_status: String,
+    #[serde(rename = "recommendedCapacity")]
+    pub recommended_capacity: Option<u32>,
+    #[serde(rename = "capacity")]
+    pub capacity: u32,
+    #[serde(rename = "previewYoutubeId")]
+    pub preview_youtube_id: Option<String>,
+    #[serde(rename = "thumbnailImageUrl")]
+    pub image_url: String,
+    #[serde(rename = "publicationDate")]
+    pub publication_date: Option<String>,
+    #[serde(rename = "updated_at")]
+    pub updated_at: Option<String>,
+    #[serde(rename = "tags")]
+    pub tags: Option<Vec<String>>,
+    #[serde(rename = "unityPackages")]
+    pub unity_packages: Option<Vec<UnityPackageFromAPI>>,
+}
