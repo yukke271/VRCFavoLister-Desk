5c9c828affb49a0e1c68fc56eec40984ac668202
yukke271 <yukke271@gmail.com>
2024-07-25 05:32:41 +0900

    update backend / create app_state.rs / create state structs

---
+++ app/vrc_favo_lister/src-tauri/src/structs/app_state.rs
@@ -0,0 +1,85 @@
+use std::{mem, sync::Mutex};
+use tauri::async_runtime::block_on;
+use sqlx::SqlitePool;
+use crate::commands::database::init_db_pool;
+use crate::structs::apiconfig::APIConfig;
+
+#[derive(Debug)]
+pub(crate) struct AppState {
+  pub(crate) context: Mutex<Context>,
+}
+
+impl AppState {
+  pub(crate) fn new() -> Self {
+    AppState {
+      context: Mutex::new(Context::new()),
+    }
+  }
+}
+
+pub(crate) trait ContextTrait {
+  fn get_api_config(&self) -> APIConfig;
+  fn set_api_config(&mut self, api_config: APIConfig);
+}
+
+#[derive(Debug)] 
+pub(crate) struct Context {
+  pub(crate) db_pool: SqlitePool,
+  api_config: APIConfig,
+}
+
+impl ContextTrait for Context {
+  fn get_api_config(&self) -> APIConfig {
+    self.api_config.clone()
+  }
+  fn set_api_config(&mut self, api_config: APIConfig) {
+    let _ = api_config.save_config_file();
+    let _ = mem::replace(&mut self.api_config, api_config);
+  }
+}
+
+impl Context {
+  pub(crate) fn new() -> Self {
+    check_app_data_dir();
+    let api_config = APIConfig::new();
+    let db_pool = block_on(init_db_pool()).expect("error while initializing database pool");
+    Context {
+      db_pool,
+      api_config,
+    }
+  }
+}
+
+
+// 設定ファイルなどを格納するディレクトリが存在しない場合、作成する
+fn check_app_data_dir() {
+  use tauri::{api::path::{BaseDirectory, resolve_path}, Env};
+
+  let context = tauri::generate_context!();
+  let app_data_dir = resolve_path(
+    context.config(),
+    context.package_info(),
+    &Env::default(),
+    "",
+    Some(BaseDirectory::AppLocalData)
+  ).unwrap();
+  if !app_data_dir.exists() {
+    std::fs::create_dir_all(app_data_dir.clone()).expect("Cannot create app data directory");
+  }
+
+  println!("App data directory: {:?}", app_data_dir.clone());
+}
+
+// TODO:ファイルパスを返す関数を作る
+// pub(crate) fn get_file_path(file_name: &str) -> String {
+//   let context = tauri::generate_context!();
+//   let app_data_dir = tauri::api::path::resolve_path(
+//     context.config(),
+//     context.package_info(),
+//     &tauri::api::path::Env::default(),
+//     "",
+//     Some(tauri::api::path::BaseDirectory::AppLocalData)
+//   ).unwrap();
+//   // PathbufをStringに変換
+//   app_data_dir.into_os_string().into_string().unwrap() + file_name;
+// }
