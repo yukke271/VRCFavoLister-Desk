be14962af13c39f416ab92e2b7bb9736ae1a2650
yukke271 <yukke271@gmail.com>
2024-07-26 06:01:40 +0900

    update backend / update module file

--- app/vrc_favo_lister/src-tauri/src/commands.rs
+++ app/vrc_favo_lister/src-tauri/src/commands.rs
@@ -1,3 +1,4 @@
 pub mod auth;
 pub mod database;
 pub mod favorite;
+pub mod utils;
--- app/vrc_favo_lister/src-tauri/src/structs.rs
+++ app/vrc_favo_lister/src-tauri/src/structs.rs
@@ -2,3 +2,6 @@ pub mod apiconfig;
 pub mod app_state;
 pub mod unity_package;
 pub mod favorite_world;
+
+pub mod db_structs;
+pub mod app_struct;
