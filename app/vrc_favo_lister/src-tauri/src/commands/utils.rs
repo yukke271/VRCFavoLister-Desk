0c804fe8aaa59011f5821462ac80589a40df7a01
yukke271 <yukke271@gmail.com>
2024-07-26 05:28:04 +0900

    update backend / update commands/utils.rs

--- app/vrc_favo_lister/src-tauri/src/commands/utils.rs
+++ app/vrc_favo_lister/src-tauri/src/commands/utils.rs
@@ -1,6 +1,31 @@
-// 細かい便利な関数をまとめたファイル
+/*
+  細かい便利な関数をまとめたファイル
+  このファイルには、以下の関数が含まれています。
+  - is_debug
+  - debug_log
+  - get_file_path
+  - get_file_path_str
+  - create_headers
+  - create_cookie_jar
+  - create_request_client
 
-use tauri::{api::path::{BaseDirectory, resolve_path}, Env, generate_context};
+*/ 
+
+
+use std::sync::Arc;
+use std::time::Duration;
+
+use reqwest::Url;
+use reqwest::Client;
+use reqwest::cookie::Jar;
+use reqwest::header::HeaderMap;
+use reqwest::header::{HeaderValue, USER_AGENT, ACCEPT_ENCODING};
+
+use tauri::api::path::{BaseDirectory, resolve_path};
+use tauri::Env;
+use tauri::generate_context;
+
+use crate::structs::apiconfig::APIConfig;
 
 // 現在のビルドステータスを取得する関数。
 // 開発者ビルドの時にtrueを返す。
@@ -52,4 +77,43 @@ pub fn get_file_path_str(file_name: &str) -> String {
   ).unwrap();
   // PathbufをStringに変換
   app_data_dir.into_os_string().into_string().unwrap() + file_name
+}
+
+// httpリクエストのヘッダーを作成する関数
+pub fn create_headers(user_agent: &str) -> HeaderMap {
+  debug_log("ヘッダー設定");
+  let mut headers: HeaderMap = HeaderMap::new();
+  headers.insert(USER_AGENT, HeaderValue::from_str(user_agent).unwrap());
+  headers.insert("Content-Type", HeaderValue::from_str("application/json").unwrap());
+  headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate, br"));
+  headers
+}
+
+// httpリクエストのクッキージャーを作成する関数
+pub fn create_cookie_jar(api_config: &APIConfig) -> Arc<Jar> {
+  debug_log("クッキージャー作成");
+  let cookie_jar = Arc::new(Jar::default());
+  cookie_jar.add_cookie_str(&("apiKey=".to_owned() + &api_config.api_key.clone()), &Url::parse(&api_config.base_url).unwrap());
+  // authCookieがある場合設定する
+  if !api_config.auth_cookie.is_none() {
+    debug_log("authCookie設定する");
+    cookie_jar.add_cookie_str(&api_config.auth_cookie.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
+  }
+  // two_factor_authがある場合設定する
+  if !api_config.two_factor_auth.is_none() {
+    debug_log("2段階認証設定");
+    cookie_jar.add_cookie_str(&api_config.two_factor_auth.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
+  }
+  cookie_jar
+}
+
+// httpリクエストクライアントを作成する関数
+pub fn create_request_client(headers: HeaderMap, cookie_jar: Arc<Jar>) -> Client {
+  debug_log("リクエストクライアント作成");
+  Client::builder()
+    .default_headers(headers)
+    .cookie_provider(cookie_jar) 
+    .timeout(Duration::from_secs(10))
+    .build()
+    .unwrap()
 }
