d3452d0826c3ac9cf6a69d5367f0e5d00096903e
yukke271 <yukke271@gmail.com>
2024-07-26 04:20:06 +0900

    update backend / update commands/auth.rs

--- app/vrc_favo_lister/src-tauri/src/commands/auth.rs
+++ app/vrc_favo_lister/src-tauri/src/commands/auth.rs
@@ -2,6 +2,7 @@ use reqwest::{Url, Client, RequestBuilder, Response, cookie::Jar, header::{self,
 use std::{sync::Arc, time::Duration};
 
 use crate::structs::app_state::{AppState, ContextTrait};
+use crate::commands::utils::debug_log;
 
 /*
 
@@ -18,14 +19,22 @@ use crate::structs::app_state::{AppState, ContextTrait};
     4: 認証アプリによる2段階認証
     5: 2段階認証のコードが空
     6: 何らかの理由でログインに失敗した
+
+    auth.rsで使用するエンドポイントは以下の通り
+    - ユーザー情報取得: https://api.vrchat.cloud/api/1/auth/user
+    - ログアウト: https://api.vrchat.cloud/api/1/auth/logout
+    - 2段階認証: https://api.vrchat.cloud/api/1/auth/twofactorauth/emailotp/verify
+    - 2段階認証: https://api.vrchat.cloud/api/1/auth/twofactorauth/totp/verify
+    - 2段階認証: https://api.vrchat.cloud/api/1/auth/twofactorauth/otp/verify
+    - Cookie確認: https://api.vrchat.cloud/api/1/auth
 */
 #[tauri::command]
 pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, password: &str, otp_code: &str) -> Result<u8, ()> { 
     
     // 引数内の文字列を表示
-    println!("username: {}", username);
-    println!("password: {}", password);
-    println!("otp_code: {}", otp_code);
+    debug_log(format!("username: {}", username));
+    debug_log(format!("password: {}", password));
+    debug_log(format!("otp_code: {}", otp_code));
 
     // APIConfigを取得
     let mut api_config = app_state.context.lock().unwrap().get_api_config();
@@ -46,31 +55,31 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
         }
     }
 
-    println!("DebugLog:ヘッダー設定");
+    debug_log("ヘッダー設定");
     // ヘッダー設定
     let mut headers: HeaderMap  = HeaderMap::new();
     headers.insert(USER_AGENT, HeaderValue::from_str(&api_config.user_agent.clone()).unwrap());
     headers.insert("Content-Type", HeaderValue::from_str("application/json").unwrap());
     headers.insert(header::ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate, br"));
 
-    println!("DebugLog:クッキージャー作成");
+    debug_log("クッキージャー作成");
     // クッキージャーを作成
     let cookie_jar = Arc::new(Jar::default());
     cookie_jar.add_cookie_str(&("apiKey=".to_owned() + &api_config.api_key.clone()), &Url::parse(&api_config.base_url).unwrap());
     
-    println!("DebugLog:authCookie設定する");
     // authCookieがある場合設定する
     if !api_config.auth_cookie.is_none() && !otp_code.is_empty() {
+        debug_log("authCookie設定する");
         cookie_jar.add_cookie_str(&api_config.auth_cookie.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
     }
 
-    println!("DebugLog:2段階認証済設定");
     // two_factor_authがある場合設定する
     if !api_config.two_factor_auth.is_none() {
+        debug_log("2段階認証設定");
         cookie_jar.add_cookie_str(&api_config.two_factor_auth.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
     }
 
-    println!("DebugLog:リクエストクライアント作成");
+    debug_log("リクエストクライアント作成");
     // リクエストクライアントを作成        
     let client: Client  = reqwest::Client::builder()
         .default_headers(headers.clone())
@@ -79,7 +88,7 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
         .build()
         .unwrap();
     
-    println!("DebugLog:リクエストビルダー作成");
+    debug_log("リクエストビルダー作成");
     // リクエスト発行準備
     // エンドポイントの設定
     let mut user_request:RequestBuilder = client
@@ -87,13 +96,13 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
         .headers(headers.clone())
         .basic_auth(api_config.username.clone().unwrap(), Some(api_config.password.clone().unwrap()));
 
-    println!("DebugLog:2段階認証コード設定");
+    debug_log("2段階認証コード設定");
     // 2段階認証のコードがある場合、postで認証
     if !otp_code.is_empty() {
 
         if api_config.two_factor_type.is_none() {
             // 2段階認証の方式が未設定の場合エラー
-            println!("DebugLog:2段階認証方式未設定");
+            debug_log("2段階認証方式未設定");
             return Ok(1);
         }
         let factor_type: String = api_config
@@ -108,9 +117,8 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
         // 2段階認証の方式によってエンドポイントが異なる
         let verify_endpoint: String;
         
-        println!("DebugLog:2段階認証方式判定");
-        println!("factor type is : \n{}", factor_type);
-        
+        debug_log("2段階認証方式判定");
+        debug_log(format!("factor type is : \n{}", factor_type));        
         if factor_type.contains("emailOtp") {
             // emailOtpの場合
             verify_endpoint = api_config.base_url.clone() + "auth/twofactorauth/emailotp/verify";
@@ -122,63 +130,48 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
             verify_endpoint = api_config.base_url.clone() + "auth/twofactorauth/otp/verify";
         } else {
             // それ以外の例外
-            println!("DebugLog:2段階認証方式不明");
-            println!("factor type is : \n{}", factor_type);
+            debug_log("2段階認証方式不明");
+            debug_log(format!("factor type is : \n{}", factor_type));
             return Ok(1);
         }
         
-        println!("DebugLog:リクエストビルダー再設定");
+        debug_log("リクエストビルダー再設定");
         user_request = client
             .post(verify_endpoint)
             .headers(headers.clone())
             .body(send_data);
     }
-    
-    println!("DebugLog:リクエスト発行");
+
+    debug_log("リクエスト発行");
     // エンドポイントと認証に必要な情報以外を設定し、リクエストの発行
     let user_response:Response = user_request
         .send()
         .await
         .unwrap();  
-        
-    // println!("DebugLog:ステータス確認");
-    // // リクエストの成否を確認
-    // if !user_response.status().is_success() {
-    //     eprintln!("Failed to first request: {}", user_response.status());
-    //     return Ok(1);
-    // }
-
-    /*
-    let cookies = user_response
-        .headers()
-        .get(SET_COOKIE)
-        .expect("failed to get cookie")
-        .to_str()
-        .unwrap();
-    println!("cookies is : \n{}", cookies);
-    */
     
-    println!("process get header");
-    // user_response.headers().get_all(&api_config.base_url).iter().for_each(|v| {
-    //     println!("header: {:?}", v.to_str().unwrap());
-    // });
+    debug_log("ステータス確認");
+    // リクエストの成否を確認
+    if !user_response.status().is_success() {
+        debug_log("Failed to first request");
+        return Ok(1);
+    }
+
+    debug_log("ヘッダー取得");
     for (header_name, header_value) in user_response.headers() {
-        // println!("{}: {:?}", header_name, header_value);
+        debug_log(format!("{}: {:?}", header_name, header_value));
         if header_name == "set-cookie" {
-            println!("set-cookie: {:?}", header_value);
+            debug_log(format!("set-cookie: {:?}", header_value));
             if header_value.to_str().unwrap().contains("authcookie") {
-                println!("authcookie: {:?}", header_value);
+                debug_log(format!("authcookie: {:?}", header_value));
                 api_config.auth_cookie = Some(header_value.to_str().unwrap_or_default().to_string());
             }
             if header_value.to_str().unwrap().contains("twoFactorAuth") {
-                println!("twoFactorAuth: {:?}", header_value);
+                debug_log(format!("twoFactorAuth: {:?}", header_value));
                 api_config.two_factor_auth = Some(header_value.to_str().unwrap_or_default().to_string());
             }
         }
     }
-    println!("success get header");
     
-
     // レスポンスの中身を取得
     // レスポンスはストリームのため一度しか処理できない
     // 複製したい場合は、text()を使い、Stringに変換してから処理する
@@ -188,26 +181,17 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
         .text()
         .await
         .unwrap();
-    println!("Response is : \n{}", body);
-    
+    debug_log(format!("Response is : \n{}", body));
+   
     // 2段階認証が必要か確認
     if body.contains("requiresTwoFactorAuth") {
     
-        /*
-        // println!("cookie: {:?}", cookies);
-        api_config.auth_cookie = Some(cookies.to_string());        
-        // authCookieが取得できなかった場合エラー
-        if api_config.auth_cookie.is_none() {
-            eprintln!("Failed to get authCookie");
-            return Ok(6);
-        }
-        */
-
         // bodyにemailOtpが含まれている場合、メールボックスの確認が必要な2段階認証
         if body.contains("emailOtp") {
             api_config.two_factor_type = Some("emailOtp".to_string());
             // Configを保存
             app_state.context.lock().unwrap().set_api_config(api_config);
+            debug_log("メールボックスの確認が必要な2段階認証");
             return Ok(3);
         }
 
@@ -216,6 +200,7 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
             api_config.two_factor_type = Some("totp".to_string());
             // Configを保存
             app_state.context.lock().unwrap().set_api_config(api_config);
+            debug_log("認証アプリによる2段階認証");
             return Ok(4);
         }
 
@@ -224,15 +209,129 @@ pub async fn login(app_state: tauri::State<'_, AppState>, username: &str, passwo
             api_config.two_factor_type = Some("totp".to_string());
             // Configを保存
             app_state.context.lock().unwrap().set_api_config(api_config);
+            debug_log("認証アプリによる2段階認証");
             return Ok(4);
         }
 
         // 上記の処理で2段階認証が必要か確認できなかった場合、完全な例外なためエラー
+        debug_log("2段階認証方式不明");
         return Ok(1);
 
     }
 
     // Configを保存
     app_state.context.lock().unwrap().set_api_config(api_config);
+    debug_log("ログイン成功");
     return Ok(0);
+}
+
+// ログアウト処理
+#[tauri::command]
+pub async fn logout(app_state: tauri::State<'_, AppState>) -> Result<u8, ()> { 
+    
+    // APIConfigを取得
+    let mut api_config = app_state.context.lock().unwrap().get_api_config();
+
+    // ヘッダー設定
+    let mut headers: HeaderMap  = HeaderMap::new();
+    headers.insert(USER_AGENT, HeaderValue::from_str(&api_config.user_agent.clone()).unwrap());
+    headers.insert("Content-Type", HeaderValue::from_str("application/json").unwrap());
+    headers.insert(header::ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate, br"));
+
+    // クッキージャーを作成
+    let cookie_jar = Arc::new(Jar::default());
+    cookie_jar.add_cookie_str(&("apiKey=".to_owned() + &api_config.api_key.clone()), &Url::parse(&api_config.base_url).unwrap());
+    cookie_jar.add_cookie_str(&api_config.auth_cookie.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
+    cookie_jar.add_cookie_str(&api_config.two_factor_auth.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
+    
+    // リクエストクライアント作成        
+    let client: Client  = reqwest::Client::builder()
+        .default_headers(headers.clone())
+        .cookie_provider(cookie_jar) 
+        .timeout(Duration::from_secs(10))
+        .build()
+        .unwrap();
+    
+    // リクエスト発行準備
+    // エンドポイントの設定
+    let user_request:RequestBuilder = client
+        .put(api_config.base_url.clone() + "auth/logout")
+        .headers(headers.clone())
+        .basic_auth(api_config.username.clone().unwrap(), Some(api_config.password.clone().unwrap()));
+
+    // リクエスト発行
+    let user_response:Response = user_request
+        .send()
+        .await
+        .unwrap();  
+    
+    // リクエストの成否を確認
+    if !user_response.status().is_success() {
+        debug_log("ログアウト失敗");
+        return Ok(1);
+    }
+
+    // Configを初期化
+    api_config.auth_cookie = None;
+    api_config.two_factor_auth = None;
+    api_config.two_factor_type = None;
+    api_config.username = None;
+    api_config.password = None;
+
+    // Configを保存
+    app_state.context.lock().unwrap().set_api_config(api_config);
+
+    debug_log("ログアウト成功");
+    return Ok(0);
+}
+
+// cookieが有効か確認
+// 有効な場合はtrueを返す
+#[tauri::command]
+pub async fn check_cookie(app_state: tauri::State<'_, AppState>) -> Result<bool, ()> { 
+    
+    // APIConfigを取得
+    let api_config = app_state.context.lock().unwrap().get_api_config();
+    
+    // ヘッダー設定
+    let mut headers: HeaderMap  = HeaderMap::new();
+    headers.insert(USER_AGENT, HeaderValue::from_str(&api_config.user_agent.clone()).unwrap());
+    headers.insert("Content-Type", HeaderValue::from_str("application/json").unwrap());
+    headers.insert(header::ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate, br"));
+
+    // クッキージャーを作成
+    let cookie_jar = Arc::new(Jar::default());
+    cookie_jar.add_cookie_str(&("apiKey=".to_owned() + &api_config.api_key.clone()), &Url::parse(&api_config.base_url).unwrap());
+    cookie_jar.add_cookie_str(&api_config.auth_cookie.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
+    cookie_jar.add_cookie_str(&api_config.two_factor_auth.clone().unwrap(), &Url::parse(&api_config.base_url).unwrap());
+    
+    // リクエストクライアント作成        
+    let client: Client  = reqwest::Client::builder()
+        .default_headers(headers.clone())
+        .cookie_provider(cookie_jar) 
+        .timeout(Duration::from_secs(10))
+        .build()
+        .unwrap();
+    
+    // リクエスト発行準備
+    // エンドポイントの設定
+    let user_request:RequestBuilder = client
+        .get(api_config.base_url.clone() + "auth")
+        .headers(headers.clone())
+        .basic_auth(api_config.username.clone().unwrap(), Some(api_config.password.clone().unwrap()));
+
+    // リクエスト発行
+    let user_response:Response = user_request
+        .send()
+        .await
+        .unwrap();  
+    
+    // リクエストの成否を確認
+    if !user_response.status().is_success() {
+        debug_log("cookieが有効ではありません");
+        return Ok(false);
+    }
+
+    debug_log("cookieが有効です");
+    return Ok(true);
 }
