6cd5baf790597905805511d93fb1cc371bb1638c
yukke271 <yukke271@gmail.com>
2024-07-26 21:56:53 +0900

    update backend / fix structs/app_struct.rs

--- app/vrc_favo_lister/src-tauri/src/structs/app_struct.rs
+++ app/vrc_favo_lister/src-tauri/src/structs/app_struct.rs
@@ -1,4 +1,6 @@
 use serde::{Serialize, Deserialize};
+use sqlx::{Row, FromRow};
+use sqlx::sqlite::SqliteRow;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AppFavoriteWorldCard {
@@ -18,12 +20,31 @@ pub struct AppFavoriteWorldCard {
     pub capacity: u32,
     #[serde(rename = "previewYoutubeId")]
     pub preview_youtube_id: Option<String>,
-    #[serde(rename = "thumbnailImageUrl")]
+    #[serde(rename = "imageId")]
     pub image_url: String,
-    #[serde(rename = "publication_date")]
+    #[serde(rename = "publicationDate")]
     pub publication_date: String,
     #[serde(rename = "updated_at")]
     pub updated_at: String,
     #[serde(rename = "platform")]
     pub platform: String,
+}
+
+impl<'r> FromRow<'r, SqliteRow> for AppFavoriteWorldCard {
+    fn from_row(row: &SqliteRow) -> Result<Self, sqlx::Error> {
+        Ok(AppFavoriteWorldCard {
+            world_id: row.try_get("id")?,
+            world_name: row.try_get("name")?,
+            description: row.try_get("description")?,
+            author_name: row.try_get("authorName")?,
+            release_status: row.try_get("releaseStatus")?,
+            recommended_capacity: row.try_get("recommendedCapacity")?,
+            capacity: row.try_get("capacity")?,
+            preview_youtube_id: row.try_get("previewYoutubeId")?,
+            image_url: row.try_get("imageId")?,
+            publication_date: row.try_get("publicationDate")?,
+            updated_at: row.try_get("updated_at")?,
+            platform: row.try_get("platform")?,
+        })
+    }
 }
